de440e94fd400cbead3682f899e0ba24123f825e
Diogo Nunes <dialex.nunes@gmail.com>
2014-02-16 16:44:10 +0000

    update analytics code
    remove analytics from cpu.html

--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-// ==== Google analytics code ====
-var _gaq=[['_setAccount','UA-10714206-5'],['_trackPageview']];
-(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-s.parentNode.insertBefore(g,s)}(document,'script'));
