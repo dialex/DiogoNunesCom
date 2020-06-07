f6b211c43df81e3ff655d66aafd344b9a6b5f4b7
Diogo Nunes <dialex.nunes@gmail.com>
2020-06-07 14:20:39 +0100

    format: prettify

--- hireme/assets/js/main.js
+++ hireme/assets/js/main.js
@@ -1,111 +1,98 @@
+$(document).ready(function () {
+  $("html").addClass("js-enabled");
 
+  setup_nivo_lightbox();
+  setup_dense();
 
+  $(window).load(function () {
+    $(".js-preloader").fadeOut(800, function () {
+      $(".js-main-container").fadeIn(800);
 
-$(document).ready(function() {
-
-    $('html').addClass('js-enabled');
-
-    setup_nivo_lightbox();
-    setup_dense();
-
-    $(window).load(function() {
-        $(".js-preloader").fadeOut(800, function() {
-            $(".js-main-container").fadeIn(800);
-
-            setup_scrollreveal();
-            setup_progress_bar_animation();
-        });
+      setup_scrollreveal();
+      setup_progress_bar_animation();
     });
-
+  });
 });
 
+function setup_progress_bar_animation() {
+  var $animation_elements = $("[class*='a-']");
+  var $window = $(window);
 
+  $window.on("scroll resize", function () {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = window_top_position + window_height;
 
-function setup_progress_bar_animation()
-{
-    var $animation_elements = $("[class*='a-']");
-    var $window = $(window);
+    $.each($animation_elements, function () {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = element_top_position + element_height;
 
-    $window.on('scroll resize', function() {
-        var window_height = $window.height();
-        var window_top_position = $window.scrollTop();
-        var window_bottom_position = (window_top_position + window_height);
+      // Check to see if this current container is within viewport
+      if (element_bottom_position >= window_top_position && element_top_position <= window_bottom_position) {
+        $element.addClass("in-view");
 
-        $.each($animation_elements, function() {
-            var $element = $(this);
-            var element_height = $element.outerHeight();
-            var element_top_position = $element.offset().top;
-            var element_bottom_position = (element_top_position + element_height);
+        // Animate progress bar
+        if ($element.hasClass("a-progress-bar")) {
+          $element.css("width", $element.attr("data-percent") + "%");
+        }
+      }
+      //else {
+      //    $element.removeClass('in-view');
+      //}
+    });
+  });
 
-            // Check to see if this current container is within viewport
-            if ((element_bottom_position >= window_top_position) &&
-                (element_top_position <= window_bottom_position)) {
-                $element.addClass('in-view');
+  $window.trigger("scroll");
+}
 
-                // Animate progress bar
-                if ($element.hasClass('a-progress-bar')) {
-                    $element.css('width', $element.attr('data-percent') + '%');
-                }
+function setup_dense() {
+  if ($.isFunction($.fn.dense)) {
+    $("img").dense({
+      glue: "@",
+    });
+  }
+}
 
-            }
-            //else {
-            //    $element.removeClass('in-view');
-            //}
-        });
+function setup_scrollreveal() {
+  if (typeof ScrollReveal !== "undefined" && $.isFunction(ScrollReveal)) {
+    window.sr = ScrollReveal();
+
+    var default_config = {
+      duration: 500,
+      delay: 0,
+      easing: "ease",
+      scale: 1,
+      mobile: false,
+    };
+    var header_config = $.extend(false, default_config, {
+      duration: 1200,
+      delay: 700,
+    });
+    var footer_config = $.extend(false, default_config, {
+      duration: 1500,
+      distance: 0,
+      viewOffset: { top: 0, right: 0, bottom: 100, left: 0 },
     });
 
-    $window.trigger('scroll');
+    var default_delay = 175;
 
+    sr.reveal(".a-header", header_config, default_delay);
+    sr.reveal(".a-footer", footer_config, default_delay);
+  }
 }
 
+function setup_nivo_lightbox() {
+  if ($.isFunction($.fn.nivoLightbox)) {
+    var $selector = $(".js-lightbox");
 
-
-function setup_dense()
-{
-    if($.isFunction($.fn.dense)) {
-
-        $('img').dense({
-            'glue': '@'
-        });
-
-    }
-}
-
-
-
-function setup_scrollreveal()
-{
-    if(typeof ScrollReveal !== 'undefined' && $.isFunction(ScrollReveal)) {
-
-        window.sr = ScrollReveal();
-
-        var default_config = {
-            duration: 500,
-            delay: 0,
-            easing: 'ease',
-            scale: 1,
-            mobile: false
-        };
-        var header_config = $.extend(false, default_config, {
-            duration: 1200,
-            delay: 700
-        });
-        var footer_config = $.extend(false, default_config, {
-            duration: 1500,
-            distance: 0,
-            viewOffset: {top: 0, right: 0, bottom: 100, left: 0}
-        });
-
-        var default_delay = 175;
-
-        sr.reveal('.a-header', header_config, default_delay);
-        sr.reveal('.a-footer', footer_config, default_delay);
-
-    }
-
-}
-
-
+    // Hide all titles to prevent tooltip from showing
+    $selector.each(function () {
+      var title = $(this).attr("title");
+      $(this).attr("data-title", title);
+      $(this).attr("title", "");
+    });
 
 function setup_nivo_lightbox()
 {
