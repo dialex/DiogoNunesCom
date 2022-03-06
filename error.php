ec8c2abbb30529178bd90f6bfb77344753f6cfec
Diogo Nunes <dialex.nunes@gmail.com>
2022-03-06 15:09:35 +0000

    Revert "chore: IE8 is no longer supported (clean up code)"
    This reverts commit 78a9b67fb3a9eecd3f2b861c9162e991967b13c9.

--- error.php
+++ error.php
@@ -27,6 +27,12 @@
     <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
     <link href="/assets/fonts/fonts.css" rel="stylesheet" type="text/css">
 
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-10714206-5"></script>
     <script>
